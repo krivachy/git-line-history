50477ef5247c0d0b1f8913d2af6fa0a6652478f9
krivachy <krivachy.akos@gmail.com>
2015-03-12 12:21:50 +0000

    Adding example test class.

---
+++ src/test/scala/org/ucl/compgs04/scm/git/GitScmTest.scala
@@ -0,0 +1,11 @@
+package org.ucl.compgs04.scm.git
+
+import org.scalatest.FlatSpec
+
+
+class GitScmTest extends FlatSpec {
+  val scm = new GitScm
+  "Git Scm" should "run diff correctly" in {
+    // TODO
+  }
+}
