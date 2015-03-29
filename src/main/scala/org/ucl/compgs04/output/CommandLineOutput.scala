b377c7c2be0831dd422bc55f11285ad70a27cfdc
krivachy <krivachy.akos@gmail.com>
2015-03-29 02:47:38 +0100

    A few command line output changes.

--- src/main/scala/org/ucl/compgs04/output/CommandLineOutput.scala
+++ src/main/scala/org/ucl/compgs04/output/CommandLineOutput.scala
@@ -3,17 +3,22 @@ package org.ucl.compgs04.output
 import org.ucl.compgs04.model.FileLineHistory
 
 object CommandLineOutput extends Output {
+
+  private val lineSeparator = System.getProperty("line.separator")
   /**
    * Takes a file history and outputs it to wherever (command line/file/etc.)
    * @param fileHistory - the result
    */
   override def processToOutput(fileHistory: FileLineHistory): Unit = {
     val lineHis = new StringBuilder
-    fileHistory.lineHistory.foreach{
-      line =>
-        lineHis.append(line.history.map(hash => hash.hash).mkString(" "))
-        lineHis.append(": ").append(line.originalLine.line)
-        lineHis.append(System.getProperty("line.separator"))
+    fileHistory.lineHistory.toList match {
+      case Nil => lineHis.append(s"File '${fileHistory.fileName}' has no history.")
+      case history =>
+        history.foreach { line =>
+          lineHis.append(line.history.map(hash => hash.hash.take(7)).mkString(" "))
+          lineHis.append(": ").append(line.originalLine.line)
+          lineHis.append(lineSeparator)
+        }
     }
     print(lineHis)
   }
