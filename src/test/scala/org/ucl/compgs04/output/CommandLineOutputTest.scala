96cd92dbd34b8ef1c9e37a8e656e0e2d4ed166ef
AUnterrainer <unterrainer.alexander@hotmail.com>
2015-03-29 20:38:31 +0100

    Deleted unnecessary comments

--- src/test/scala/org/ucl/compgs04/output/CommandLineOutputTest.scala
+++ src/test/scala/org/ucl/compgs04/output/CommandLineOutputTest.scala
@@ -3,9 +3,7 @@ package org.ucl.compgs04.output
 import org.scalatest.FlatSpec
 import org.ucl.compgs04.model._
 
-/**
- * Created by Alexander on 12/03/15.
- */
+
 class CommandLineOutputTest extends FlatSpec {
   "CommandlineOutput" should "output the values in the command line" in {
     val test = FileLineHistory("TestFile", Seq(
